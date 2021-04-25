072353e6dcf4e25b3ceb517e40ca9d2d7caf91c0
Maya Santiago <msatori.code@gmail.com>
2021-04-24 20:53:49 -0500

    created basic hero to be used for pages

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,22 +1,12 @@
-import { React } from 'react'; 
-
+import { React } from 'react';
+import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 
 export default function Services() {
     return (
         <section className="service-wrapper">
-            <div className="service-content">
-                <h1>Services</h1>
-                <p>What could be better than fresh, organic flowers that are locally grown?
-                    The mission at Flowers for the People is to provide the joy of flowers to everyone. 
-                </p>
-                <p>I happily provide the following services:</p>
-                <ul>
-                    <li>Wedding Arrangments and Bridal Boquets</li>
-                    <li>Private Events</li>
-                    <li>Bi-weekly and Weekly CSA</li>
-                </ul>
-            </div>
-            
+                    <div className="mask">
+                            <h1>Services</h1>
+                    </div>
         </section>
     )
 };
