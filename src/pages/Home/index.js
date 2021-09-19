5cec3a6fadd672f616b388cc530a0c10f5faf177
Maya Santiago <msatori.code@gmail.com>
2021-09-19 16:44:44 -0500

    installed css for mdbreact, configured jumbot

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,8 +3,8 @@ import {
     BrowserRouter as Router,
     Link
 } from "react-router-dom";
-import { MDBContainer, MDBJumbotron } from 'mdbreact'
-import logo from '../../assets/images/logo.png';
+import { MDBContainer, MDBJumbotron, MDBBtn, MDBCol, MDBRow, MDBCardTitle, MDBIcon } from 'mdbreact'
+import bg from '../../assets/images/bg.JPG';
 
 //page imports
 import Contact from '../Contact';
@@ -12,25 +12,22 @@ import Services from '../Services';
 export default function Home() {
     return (
         <section>
-            <MDBContainer className="text-center">
-                <div className="home">
-                    <MDBJumbotron>
-                        <div className='mask'>
-                            <div className="cta">
-                                <h1>Flowers 4 the People</h1>
-                                <div className="cta-nav">
-                                    <ul>
-                                        <li>
-                                            <Link to="/contact">Contact</Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </MDBJumbotron>
-                </div>
+            <MDBContainer fluid>
+                <MDBRow>
+                    <MDBCol>
+                        <MDBJumbotron>
+                            <MDBCol className="text-white text-center py-5 px-4 my-5 mask">
+                                <MDBCol className="py-5k">
+                                    <h1>Flowers 4 the People</h1>
+                                    <MDBBtn outline color="white" className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon>
+                                        <Link to='/contact'>Contact Me</Link>
+                                    </MDBBtn>
+                                </MDBCol>
+                            </MDBCol>
+                        </MDBJumbotron>
+                    </MDBCol>
+                </MDBRow>
             </MDBContainer>
-
         </section>
     )
 }
